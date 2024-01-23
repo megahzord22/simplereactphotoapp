bf7b2cbc02b8c13c038d6b61dd722c9b5c9810ab
Megan Grant <granmega@oregonstate.edu>
2024-01-22 21:31:23 -0800

    progress

--- src/index.js
+++ src/index.js
@@ -1,9 +1,41 @@
 import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 // main app
-
+function App() {
+    return (
+        <div className="app">
+            <div className="column"></div>
+            <div className="column"></div>
+        </div>
+    )
+}
 // button
 const AppButton = <button>Generate Images</button>
 // dialogue box
-
+const Dialogue = <form>
+    <input></input>
+    <input></input>
+    </form>
+function showDialogue(Dialogue) {
+    if (Dialogue) {
+        return Dialogue
+    } else {
+        return null
+    }
+}
 // card
+function Card(props) {
+    return <div className="card">
+        {props.children}
+       // <img></img>
+       // <p></p>
+       // <button>X</button>
+    </div>
+}
+const imageList = []
+
+// root stuff 
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(AppButton)
